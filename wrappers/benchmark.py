8c09c085126cdec2d823f310e56ad41dfd1b1e33
ArslanYadov <arsyy90@gmail.com>
2022-12-01 00:09:30 +0300

    add logging in benchmark decorator

--- wrappers/benchmark.py
+++ wrappers/benchmark.py
@@ -1,9 +1,27 @@
 def benchmark(func):
     """Декоратор, показывает время работы функции."""
+    import logging
     from datetime import datetime
     def wrapper(*args, **kwargs):
         start = datetime.now()
         res = func(*args, **kwargs)
-        print(func.__name__, datetime.now() - start)
+        logging.info(
+            f'Function name: \"{func.__name__}\"; '
+            f'Time: {datetime.now() - start}.'
+        )
         return res
     return wrapper
+
+
+@benchmark
+def list_with_big_values():
+    return [int(x**x) for x in range(10000, 20000)]
+
+
+if __name__ == '__main__':
+    import logging
+    logging.basicConfig(
+        level=logging.INFO,
+        format='[%(levelname)s] : %(message)s'
+    )
+    list_with_big_values()
