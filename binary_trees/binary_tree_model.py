637d59fed8546378ab3813413025bc2d6a9a814e
ArslanYadov <arsyy90@gmail.com>
2023-03-30 20:31:28 +0300

    add insert method for bst

--- binary_trees/binary_tree_model.py
+++ binary_trees/binary_tree_model.py
@@ -5,3 +5,23 @@ class Node:
         self.value = value
         self.left = left
         self.right = right
+
+    def insert(self, value) -> None:
+        if not self.value:
+            self.value = value
+            return
+        
+        if value == self.value:
+            return
+               
+        if value < self.value:
+            if self.left:
+                self.left.insert(value)
+                return
+            self.left = Node(value)
+            return
+        
+        if self.right:
+            self.right.insert(value)
+            return
+        self.right = Node(value)
