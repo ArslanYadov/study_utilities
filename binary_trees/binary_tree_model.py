b0e0f568c7301b6408c76588f1890a3893e488ed
ArslanYadov <arsyy90@gmail.com>
2023-03-30 18:40:47 +0300

    sep binary tree model

---
+++ binary_trees/binary_tree_model.py
@@ -0,0 +1,7 @@
+class Node:
+    """Простая модель вершины бинарного дерева."""
+
+    def __init__(self, value=0, left=None, right=None) -> None:
+        self.value = value
+        self.left = left
+        self.right = right
