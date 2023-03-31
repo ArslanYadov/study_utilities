4304f5600d35473b801a1ba3a78f832b6efc16f1
ArslanYadov <arsyy90@gmail.com>
2023-03-31 21:48:14 +0300

    add remove method

--- binary_trees/binary_tree_model.py
+++ binary_trees/binary_tree_model.py
@@ -5,25 +5,6 @@ class Node:
         self.value = value
         self.left = left
         self.right = right
-
-    def insert(self, value) -> None:
-        """Вставка нового узла в дерево."""
-        if not self.value:
-            self.value = value
-            return
-
-        if value < self.value:
-            if self.left:
-                self.left.insert(value)
-                return
-            self.left = Node(value)
-            return
-
-        if value >= self.value:
-            if self.right:
-                self.right.insert(value)
-                return
-            self.right = Node(value)
     
     def find_min(self):
         """Поиск минимального узла."""
@@ -36,3 +17,51 @@ class Node:
         if not self.right:
             return self
         return self.right.find_max()
+    
+    def remove(self, key):
+        """Удаление узла из дерева."""
+        if not self:
+            return self
+
+        if key < self.value:
+            self.left = self.left.remove(key)
+
+        elif key >= self.value:
+            self.right = self.right.remove(key)
+        
+        elif self.left and self.right:
+            self.value = self.right.find_min().value
+            self.right = self.right.remove(self.value)
+        
+        else:
+            if self.left:
+                self = self.left
+            elif self.right:
+                self = self.right
+            else:
+                self = None
+
+        return self
+
+
+    def insert(self, key) -> None:
+        """Вставка нового узла в дерево."""
+        if not self.value:
+            self.value = key
+            return
+
+        if key < self.value:
+            if self.left:
+                self.left.insert(key)
+                return
+            self.left = Node(key)
+            return
+
+        if key >= self.value:
+            if self.right:
+                self.right.insert(key)
+                return
+            self.right = Node(key)
+    
+
+    
