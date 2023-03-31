59efdd7987fd809cc87363db834ed93e8f86a2c5
ArslanYadov <arsyy90@gmail.com>
2023-03-31 11:52:28 +0300

    whitespaces :/

--- binary_trees/binary_tree_model.py
+++ binary_trees/binary_tree_model.py
@@ -1,26 +1,27 @@
 class Node:
     """Простая модель вершины бинарного дерева."""
 
-    def __init__(self, value=0, left=None, right=None) -> None:
+    def __init__(self, value=None, left=None, right=None) -> None:
         self.value = value
         self.left = left
         self.right = right
 
     def insert(self, value) -> None:
+        """Вставка нового узла в дерево."""
         if not self.value:
             self.value = value
             return
-        
+
         if value == self.value:
             return
-               
+
         if value < self.value:
             if self.left:
                 self.left.insert(value)
                 return
             self.left = Node(value)
             return
-        
+
         if self.right:
             self.right.insert(value)
             return
