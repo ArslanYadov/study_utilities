127299802a5704b2c44a6f54cb0c0c46e64e7400
ArslanYadov <arsyy90@gmail.com>
2023-03-31 22:52:01 +0300

    add annotations for return value; test cases for bst

--- binary_trees/binary_tree_model.py
+++ binary_trees/binary_tree_model.py
@@ -1,24 +1,27 @@
+from __future__ import annotations
+
+
 class Node:
-    """Простая модель вершины бинарного дерева."""
+    """Простая модель бинарного дерева."""
 
     def __init__(self, value=None, left=None, right=None) -> None:
         self.value = value
         self.left = left
         self.right = right
     
-    def find_min(self):
+    def find_min(self) -> Node:
         """Поиск минимального узла."""
         if not self.left:
             return self
         return self.left.find_min()
 
-    def find_max(self):
+    def find_max(self) -> Node:
         """Поиск максимального узла."""
         if not self.right:
             return self
         return self.right.find_max()
     
-    def remove(self, key):
+    def remove(self, key) -> Node:
         """Удаление узла из дерева."""
         if not self:
             return self
@@ -26,7 +29,7 @@ class Node:
         if key < self.value:
             self.left = self.left.remove(key)
 
-        elif key >= self.value:
+        elif key > self.value:
             self.right = self.right.remove(key)
         
         elif self.left and self.right:
@@ -64,4 +67,15 @@ class Node:
             self.right = Node(key)
     
 
+if __name__ == '__main__':
+    from searching_key import searching_key
+
+    root: Node = Node()
+    keys: tuple = (8, 3, 10, 1, 6, 14, 4, 7, 13)
+
+    for key in keys:
+        root.insert(key)
     
+    root = root.remove(3)
+
+    assert not searching_key(root, 3)
