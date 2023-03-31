52d7368c1258bd5ccc174d3e39cdaf75494fb356
ArslanYadov <arsyy90@gmail.com>
2023-03-31 12:32:37 +0300

    find key in BST

---
+++ binary_trees/searching_key.py
@@ -0,0 +1,24 @@
+from binary_tree_model import Node
+
+
+def searching_key(root: Node, key: int) -> bool:
+    """Поиск ключа в дереве."""
+    if not root:
+        return False
+    if key == root.value:
+        return True
+    return (
+        searching_key(root.left, key) if key < root.value
+        else searching_key(root.right, key)
+    )
+
+
+if __name__ == '__main__':
+
+    root: Node = Node()
+    data: tuple = (5, 3, 1, 4, 8, 6)
+    for val in data:
+        root.insert(val)
+    
+    assert searching_key(root, 4)
+    assert not searching_key(root, 2)
