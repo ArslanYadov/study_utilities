547cccd7082dcb60f8ebcb83ea8cb8c96623ff38
ArslanYadov <arsyy90@gmail.com>
2023-06-13 15:24:01 +0300

    tests for pop item from front and back in deque

--- double_linked_list/deque_test.py
+++ double_linked_list/deque_test.py
@@ -9,12 +9,12 @@ class DequeTest(unittest.TestCase):
         super().setUp()
         self.deque: Deque = Deque()
 
-    def test_deque_is_empty(self):
+    def test_deque_is_empty(self) -> None:
         """Testing for empty deque."""
         self.assertTrue(self.deque.is_empty())
         self.assertEqual(len(self.deque), 0)
 
-    def test_deque_length_after_push_back(self):
+    def test_deque_length_after_push_back(self) -> None:
         """Check length after push_back method for deque."""
         for i in range(1, 11):
             self.deque.push_back(i)
@@ -22,13 +22,33 @@ class DequeTest(unittest.TestCase):
         self.assertFalse(self.deque.is_empty())
         self.assertEqual(len(self.deque), 10)
 
-    def test_deque_length_after_push_front(self):
+    def test_deque_length_after_push_front(self) -> None:
         """Check length after push_front method for deque."""
         for i in range(1, 11):
             self.deque.push_front(i)
 
         self.assertFalse(self.deque.is_empty())
         self.assertEqual(len(self.deque), 10)
+
+    def test_pop_item_from_end(self) -> None:
+        """Testing pop item from the end of deque."""
+        items: list = list(range(1, 11))
+
+        for item in items:
+            self.deque.push_back(item)
+
+        for expected in items[::-1]:
+            self.assertEqual(self.deque.pop_back(), expected)
+
+    def test_pop_item_from_front(self) -> None:
+        """Testing pop item from the front of deque."""
+        items: list = list(range(1, 11))
+
+        for item in items:
+            self.deque.push_back(item)
+
+        for expected in items:
+            self.assertEqual(self.deque.pop_front(), expected)
 
 
 if __name__ == '__main__':
