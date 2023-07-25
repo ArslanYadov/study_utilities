feee71fe2713c3090d8f2c9dd2f100a9fb6c6ab3
ArslanYadov <arsyy90@gmail.com>
2023-07-25 23:47:37 +0300

    constants with ANSI escape codes

---
+++ async_stream_handler/global_conf.py
@@ -0,0 +1,13 @@
+_SAVE_POS: str = '\0337'
+
+_RESTORE_POS: str = '\0338'
+
+_MOVE_TOP: str = '\033[H'
+
+_DEL_LINE: str = '\033[2K'
+
+_CLEAR_LINE: str = _DEL_LINE + '\033[0G'
+
+_BACKS_CHAR: str = '\033[1D'
+
+_MOVE_BOTTOM: str = '\033[<row>E'
